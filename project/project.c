7e255317384e1dbf228815ecefb3815d2dfe2c23
amirparsaro <amirparsarouhani@gmail.com>
2025-01-10 12:04:45 +0330

    user Modification complete

--- project/project.c
+++ project/project.c
@@ -16,12 +16,11 @@ struct user
     char password[50];
     char naghsh[10];
     char reshte[50];
-    struct user *next;
 };
 
 int main()
 {
-    printf("be samane modiriate ketabkhane khosh amadid. dar soorate tamayol be khorooj ctrl+c ra feshar dahid.\n");
+    printf("be samane modiriate ketabkhane khosh amadid.\n");
     int running = 1;
     char page[50] = "logIn";
     char type[50];
@@ -30,8 +29,9 @@ int main()
     {
         if (strcmp(page, "logIn") == 0)
         {
-
+            printf("\n\n\n\n\n");
             printf("==================================== log-in ====================================\n");
+            printf("baraye khorooj az barname \"0\" ra type konid.\n");
             printf("Naghshe khod ra entekhab konid: ketabdar(1) ya daneshjoo(2): ");
             scanf("%s", type);
 
@@ -107,6 +107,11 @@ int main()
                     }
                 }
             }
+            else if (strcmp(type, "0") == 0)
+            {
+                printf("mamnoonim ke az barname estefade kardid, rooze khoobi dashte bashid!\n");
+                return 0;
+            }
             else
             {
                 printf("entekhab na motabar! dobare emtehan konid.\n");
@@ -120,25 +125,43 @@ int main()
             if (strcmp(type, "1") == 0)
             {
                 printf("ketabdar aziz, (%s) entekhab konid:\n", user);
-                printf("1. namayesh list ketab ha\n");
-                printf("2. namayesh list aaza va tarikhche daryaft ketab\n");
+                printf("1. namayesh list aaza, eijade taghir dar aaza va namayesh tarikhche daryaft ketab\n");
+                printf("2. namayesh list ketab ha\n");
+                printf("3. namayesh list ketab haye darkhasti\n");
+                printf("4. \n");
+                printf("baraye log-out shodan, \"-1\" ra vared konid.\n");
+                printf("baraye khorooj az barname, \"0\" ra vared konid.\n");
 
                 int choice;
                 scanf("%d", &choice);
 
                 if (choice == 1)
                 {
+                    strcpy(page, "modifyUsers");
                 }
                 else if (choice == 2)
                 {
-                    strcpy(page, "modifyUsers");
+                }
+                else if (choice == 0)
+                {
+                    printf("mamnoonim ke az barname estefade kardid, rooze khoobi dashte bashid!\n");
+                    return 0;
+                }
+                else if (choice == -1)
+                {
+                    printf("ketabdar aziz (%s), shoma az system log-out shodid.\n", user);
+                    strcpy(page, "logIn");
                 }
                 else
                 {
                     printf("entekhab na motabar! dobare emtehan konid.\n");
                 }
             }
+            else if (strcmp(type, "2") == 0)
+            {
+            }
         }
+
         else if (strcmp(page, "modifyUsers") == 0)
         {
             printf("\n\n\n\n\n\n\n\n\n\n\n");
@@ -148,16 +171,16 @@ int main()
 
             while (fscanf(studentFile, "%s\n%s", karbar.username, karbar.password) != EOF)
             {
-                printf("username: %s\npassword: %s\nactions| info, delete\n\n", karbar.username, karbar.password);
+                printf("username: %s\npassword: %s\nactions| info, books, promote, delete\n\n", karbar.username, karbar.password);
             }
             fclose(studentFile);
 
             printf("action ra entekhab konid, type konid: \"<action> <username>\"\n");
             printf("ya agar mikhahid user ezafe konid, type konid: \"add\"\n");
-            printf("agar mikhahid be menu asli bargardid, type konid: menu\n");
+            printf("agar mikhahid be menu asli bargardid, type konid: \"menu\"\n");
             char un[50];
             char action[50];
-            scanf("%s", &action);
+            scanf("%s", action);
             if (strcmp(action, "menu") == 0)
             {
                 strcpy(page, "menu");
@@ -177,7 +200,7 @@ int main()
             }
             else
             {
-                scanf("%s", &un);
+                scanf("%s", un);
                 if (strcmp(action, "info") == 0)
                 {
                     printf("info\n");
@@ -205,6 +228,40 @@ int main()
                         fprintf(studentFile2, "%s\n%s\n", users[j].username, users[j].password);
                     }
                     fclose(studentFile2);
+                }
+                else if (strcmp(action, "promote") == 0)
+                {
+                    printf("Amaliate promote user be ketabdar aghaz shod...\n");
+                    FILE *studentFile = fopen("login_student.txt", "r");
+                    struct user users[1000]; // change to linked list
+                    struct user current;
+                    struct user promote;
+                    int i = 0;
+                    while (fscanf(studentFile, "%s\n%s", current.username, current.password) != EOF)
+                    {
+                        if (strcmp(current.username, un) != 0)
+                        {
+                            strcpy(users[i].username, current.username);
+                            strcpy(users[i].password, current.password);
+                            i++;
+                        }
+                        else
+                        {
+                            strcpy(promote.username, current.username);
+                            strcpy(promote.password, current.password);
+                        }
+                    }
+                    fclose(studentFile);
+                    FILE *studentFile2 = fopen("login_student.txt", "w");
+                    for (int j = 0; j < i; j++)
+                    {
+                        fprintf(studentFile2, "%s\n%s\n", users[j].username, users[j].password);
+                    }
+                    fclose(studentFile2);
+                    FILE *librarianFile = fopen("login_librarian.txt", "a");
+                    fprintf(librarianFile, "%s\n%s\n", promote.username, promote.password);
+                    printf("user be ketabdar promote shod!");
+                    fclose(librarianFile);
                 }
                 else
                 {
